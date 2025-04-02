793111d25677acc343f0e6f7f9b6e13ba5a16fe6
Ningaro <23503775+ningaro@users.noreply.github.com>
2025-04-02 21:50:53 +0300

    🐛 Change to host matching

--- src/pac.ts
+++ src/pac.ts
@@ -7,7 +7,7 @@ const PROXY = "SOCKS5 127.0.0.1:1086;" // Default MacOS ss-local SOCKS5 address
 const DIRECT = "DIRECT;"
 
 function FindProxyForURL(url: string, host: string) {
-  if (urls.every((reg) => url.match(reg) === null)) {
+  if (urls.every((reg) => host.match(reg) === null)) {
     return DIRECT
   }
   return PROXY
