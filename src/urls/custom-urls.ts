87fb334888a5b923027816ffa01a6e64dd27ead6
Ningaro <23503775+ningaro@users.noreply.github.com>
2025-04-03 00:40:07 +0300

    🗃️ Remove site with access from RU

--- src/urls/custom-urls.ts
+++ src/urls/custom-urls.ts
@@ -2,9 +2,4 @@ export const customUrls: RegExp[] = [
   /ant.design$/,
   /diffcheck.io$/,
   /gemini.google.com$/,
-  /motion.dev$/,
-  /shadowsocks.org$/,
-  /steamdb.info$/,
-  /steamtime.info$/,
-  /wastedtime.io$/,
 ]
